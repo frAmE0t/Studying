ce86b2b1ed00bc9601e46f5dba5cfad01403a371
Yauheni Charniauski <chernyavskij.z@mail.ru>
2024-10-04 12:56:47 +0300

    Run methods synchronously

--- Exercise02/obj/Debug/net8.0/Exercise02.AssemblyInfo.cs
+++ Exercise02/obj/Debug/net8.0/Exercise02.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcc36a84abb30449f01aa3a85be8ad0e155d5911")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a67ff27503d76d3a37b326f545674f8f48d59f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ WorkingWithTasks/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+OutputThreadInfo();
+
+Stopwatch timer = Stopwatch.StartNew();
+
+Console.WriteLine("Running methods synchronously on one thread.");
+
+MethodA();
+MethodB();
+MethodC();
+
+Console.WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elaplsed.");
+
+static void OutputThreadInfo()
+{
+    Thread t = Thread.CurrentThread;
+
+    Console.WriteLine($"Thread Id: {t.ManagedThreadId}, Priority: {t.Priority}, Background: {t.IsBackground}, Name: {t.Name ?? "null"}");
+}
+
+static void MethodA()
+{
+    Console.WriteLine("Starting Method A...");
+    OutputThreadInfo();
+    Thread.Sleep(3000);
+    Console.WriteLine("Finished Method A.");
+}
+
+static void MethodB()
+{
+    Console.WriteLine("Starting Method B...");
+    OutputThreadInfo();
+    Thread.Sleep(2000);
+    Console.WriteLine("Finished Method B.");
+}
+
+static void MethodC()
+{
+    Console.WriteLine("Starting Method C...");
+    OutputThreadInfo();
+    Thread.Sleep(1000);
+    Console.WriteLine("Finished Method C.");
+}
