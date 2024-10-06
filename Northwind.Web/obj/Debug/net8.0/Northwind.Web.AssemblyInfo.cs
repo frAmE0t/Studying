e67d99a5038fbfb350ebd7f0125a152d1b6a6665
Yauheni Charniauski <chernyavskij.z@mail.ru>
2024-10-06 19:19:30 +0300

    Create wwwroot folder and add into it index.html

--- Northwind.Web/obj/Debug/net8.0/Northwind.Web.AssemblyInfo.cs
+++ Northwind.Web/obj/Debug/net8.0/Northwind.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Northwind.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b8c88495b892d9b7b044c08774be2b549be55a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec7dbc686a3d5a8c75e470255f81ffb60802c372")]
 [assembly: System.Reflection.AssemblyProductAttribute("Northwind.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Northwind.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
