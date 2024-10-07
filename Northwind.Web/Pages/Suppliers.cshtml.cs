e16a4eb67f71d9656a10fe0bdb7cf28e867546f0
Yauheni Charniauski <chernyavskij.z@mail.ru>
2024-10-07 11:36:24 +0300

    Use code-behind files with Razor Pages

---
+++ Northwind.Web/Pages/Suppliers.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Northwind.Web.Pages
+{
+    public class SuppliersModel : PageModel
+    {
+        public IEnumerable<string>? Suppliers { get; set; }
+
+        public void OnGet()
+        {
+            ViewData["Title"] = "Northwind B2B - Suppliers";
+
+            Suppliers = new[]
+            {
+                "Alpha Co", "Beta Limited", "Gamme Corp"
+            };
+        }
+    }
+}
--- Northwind.Web/obj/Debug/net8.0/Northwind.Web.AssemblyInfo.cs
+++ Northwind.Web/obj/Debug/net8.0/Northwind.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Northwind.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dfd483281fc05203137bf44364cac34390fa621")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+441980534d53b37b5dba07c527b9a1fd50f90ee9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Northwind.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Northwind.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
